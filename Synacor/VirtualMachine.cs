46508ab77ba8a0d93f7d7a16eaa3b35769e31c1a
Christophe Savard <christophe_savard@hotmail.ca>
2026-09-13 03:35:16 -0400

    Cleanup project files

--- Synacor/VirtualMachine.cs
+++ Synacor/VirtualMachine.cs
@@ -1,6 +1,12 @@
 ﻿namespace Synacor;
 
+/// <summary>
+/// Virtual Machine implementation
+/// </summary>
 public class VirtualMachine
 {
+    /// <summary>
+    /// Hello, World!
+    /// </summary>
     public void SayHello() => Console.WriteLine("Hello, world!");
 }
