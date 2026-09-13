73bf61c0a17b9d16cfcbc5bc305fa6a43348452a
Christophe Savard <christophe_savard@hotmail.ca>
2026-09-13 04:39:11 -0400

    Add basic command structure for CLI interface

---
+++ Synacor.CLI/SynacorCommand.cs
@@ -0,0 +1,33 @@
+﻿using DotMake.CommandLine;
+using Microsoft.Extensions.Logging;
+
+namespace Synacor.CLI;
+
+/// <summary>
+/// Synacor Challenge Virtual Machine interface
+/// </summary>
+[CliCommand(Description = "Synacor Challenge Virtual Machine interface")]
+public class SynacorCommand(ILoggerFactory factory) : ICliRunAsyncWithContextAndReturn
+{
+    private readonly ILoggerFactory factory = factory;
+
+    /// <summary>
+    /// Logger instance
+    /// </summary>
+    private ILogger Logger { get; } = factory.CreateLogger<SynacorCommand>();
+
+    /// <summary>
+    /// VM data file
+    /// </summary>
+    [CliArgument(Description = "VM data file", Arity = CliArgumentArity.ExactlyOne,
+                 ValidationRules = CliValidationRules.ExistingFile, ValidationPattern = @".+\.bin")]
+    public required FileInfo Data { get; set; }
+
+    /// <inheritdoc />
+    public async Task<int> RunAsync(CliContext cliContext)
+    {
+        VirtualMachine vm = new(this.Data, this.factory.CreateLogger<VirtualMachine>());
+        vm.SayHello();
+        return 0;
+    }
+}
--- Synacor/VirtualMachine.cs
+++ Synacor/VirtualMachine.cs
@@ -1,12 +1,35 @@
-﻿namespace Synacor;
+﻿using Microsoft.Extensions.Logging;
+
+namespace Synacor;
 
 /// <summary>
 /// Virtual Machine implementation
 /// </summary>
 public class VirtualMachine
 {
+    private readonly FileInfo data;
+
+    /// <summary>
+    /// Logger instance
+    /// </summary>
+    private ILogger Logger { get; }
+
+    /// <summary>
+    /// Creates a new Virtual Machine
+    /// </summary>
+    /// <param name="data">VM Data file</param>
+    /// <param name="logger">Logger instance</param>
+    /// <exception cref="ArgumentException">If <paramref name="data"/> does not exist</exception>
+    public VirtualMachine(FileInfo data, ILogger<VirtualMachine> logger)
+    {
+        if (!data.Exists) throw new ArgumentException("Data file does not exist", nameof(data));
+
+        this.data = data;
+        this.Logger = logger;
+    }
+
     /// <summary>
     /// Hello, World!
     /// </summary>
-    public void SayHello() => Console.WriteLine("Hello, world!");
+    public void SayHello() => this.Logger.LogInformation("Hello, World!\nData file: {Path}", this.data.FullName);
 }
