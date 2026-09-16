9a93f5a2add2b8c39c67df6f94a537f16087581b
Christophe Savard <christophe_savard@hotmail.ca>
2026-09-15 21:39:11 -0400

    Add load/save state dumps

--- Synacor.CLI/SynacorCommand.cs
+++ Synacor.CLI/SynacorCommand.cs
@@ -18,11 +18,17 @@ public partial class SynacorCommand(ILoggerFactory factory, ConsoleInputProvider
     private ILogger Logger { get; } = factory.CreateLogger<SynacorCommand>();
 
     /// <summary>
-    /// VM data file
+    /// Virtual Machine binaries file
     /// </summary>
-    [CliArgument(Description = "VM data file", Arity = CliArgumentArity.ExactlyOne,
-                 ValidationRules = CliValidationRules.ExistingFile, ValidationPattern = @".+\.bin")]
+    [CliArgument(Description = "Virtual Machine binaries file", Arity = CliArgumentArity.ExactlyOne,
+                 ValidationRules = CliValidationRules.ExistingFile)]
     public required FileInfo Data { get; set; }
+
+    /// <summary>
+    /// If the file being loaded is a Virtual Machine state and not simple binaries
+    /// </summary>
+    [CliOption(Description = "If the file being loaded is a Virtual Machine state and not simple binaries", Arity = CliArgumentArity.ZeroOrOne)]
+    public bool FromState { get; set; }
 
     /// <inheritdoc />
     public async Task<int> RunAsync(CliContext cliContext)
@@ -32,7 +38,25 @@ public partial class SynacorCommand(ILoggerFactory factory, ConsoleInputProvider
         {
             LogCreateVM(this.Logger);
             using VirtualMachine vm = new(this.factory.CreateLogger<VirtualMachine>(), this.inputProvider, outputProvider);
-            await vm.LoadFile(this.Data, cliContext.CancellationToken);
+
+            // Dump data when aborted
+            Console.CancelKeyPress += (_, _) =>
+            {
+                // ReSharper disable AccessToDisposedClosure
+                vm.Abort();
+                vm.DumpStateToFile(new FileInfo(this.Data + ".vmd")).GetAwaiter().GetResult();
+                // ReSharper restore AccessToDisposedClosure
+            };
+
+            // Load data
+            if (this.FromState)
+            {
+                await vm.LoadStateFromFile(this.Data, cliContext.CancellationToken);
+            }
+            else
+            {
+                await vm.LoadFile(this.Data, cliContext.CancellationToken);
+            }
 
             LogRunVM(this.Logger);
             int result = await vm.Run(cliContext.CancellationToken);
